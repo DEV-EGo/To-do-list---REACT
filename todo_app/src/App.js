524d076aac2b9743049883ff26b5177391bc9295
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-20 23:09:40 -0500

    const

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -2,26 +2,34 @@ import React, { useState }from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk 🐶', 'take the rubbish out', 'This is a test' ]);
+  const [input, setInput] = useState('');
+  console.log('🔫', input);
 
-  const [todos, setTodos] = useState(['Take dogs for a walk 🐶', 'take the rubbish out' ]);
-
+  const addTodo =(event) => {
+    // this will work when we click the button
+    event.preventDefault();
+    console.log('👽', 'im working');
+    setTodos([...todos, input]); //this will add to the input button
+    setInput(''); // clears up the input after clicking add todo button
+  }
 
   return (
     <div className="App">
 
       <h1>Hello Devs 🚀</h1>
 
-      <input/>
+      <form>
 
-    <button>Add to do</button>
+      <input value = {input} onChange = { event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add to do</button>
 
+      </form>
 
     <ul>
       {todos.map(todo => (
         <li>{todo}</li>
       ))}
-        <li></li>
-        <li></li>
     </ul>
 
     </div>
