47230802c6ff65d32e242b6f2d522c3162351a00
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-20 22:53:41 -0500

    todo

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState }from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState(['Take dogs for a walk 🐶', 'take the rubbish out' ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Hello Devs 🚀</h1>
+
+      <input/>
+
+    <button>Add to do</button>
+
+
+    <ul>
+      {todos.map(todo => (
+        <li>{todo}</li>
+      ))}
+        <li></li>
+        <li></li>
+    </ul>
+
     </div>
   );
 }
