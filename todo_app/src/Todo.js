b0dbe66f1d03e49844fd323a320548488a40866f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-25 23:21:47 -0500

    useStyles , modal update

--- todo_app/src/Todo.js
+++ todo_app/src/Todo.js
@@ -3,8 +3,21 @@ import './Todo.css';
 import {  List, Button, ListItemAvatar, ListItem, ListItemText, Modal } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import db from './firebase.js';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width : 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2,4,3),
+    },
+}));
 
 function Todo(props) {
+    const classes = useStyles();
     const [open,setOpen] = useState(false);
 
     const handleOpen = () => {
